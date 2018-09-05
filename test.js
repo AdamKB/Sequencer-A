713c6b0fb59b152283eb96f3bc6d4748c6af76a5
Adam Brown <42836100+AdamKB@users.noreply.github.com>
2018-09-05 16:13:08 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
+var sound = new Howl({
+  src: ['https://raw.githubusercontent.com/AdamKB/Sequencer-A/master/Bongo1.wav']
+});
+
     $(".fa-cube").on("click",function() {
       sound.play();
       swal("bugger");
